5d2e1638a98aebb1ab368f160b756fbf37099ee9
AChaoZJU <zhangshichao628@sin.com>
2023-11-20 14:16:32 +0800

    extract specs

--- utils/code.ts
+++ utils/code.ts
@@ -1,9 +1,9 @@
 import {goThroughFiles} from "./index";
 import {getFilesWithPrefix} from "./file";
 
-export const getSpecByTCId = (tcId: string) => {
+export const getFeatureScenarioByTCId = (tcId: string): string => {
   const dirs =  getFilesWithPrefix(['features'])
-  let spec = ''
+  let featureScenario = ''
 
   const getTCPart = (file: string, fileContent: string) => {
     if (fileContent.match(new RegExp(tcId, 'g'))){
@@ -11,19 +11,29 @@ export const getSpecByTCId = (tcId: string) => {
       const tcPart = fileContent.split('Scenario').slice(1).find((part: string) => {
         return part.match(new RegExp(`[${tcId}]`, 'g'))
       })
-      spec =  'Scenario' + tcPart?.split('\n\n').slice(0,1)
+      featureScenario =  'Scenario' + tcPart?.split('\n\n').slice(0,1)
     }
   }
   goThroughFiles(dirs, getTCPart)
-  return spec
+  return featureScenario
 }
 
-export const getStepsBySpecLines = (specLines: string[]) => {
+export const getStepsBySpecLines = (featureScenario: string) => {
   const dirs =  getFilesWithPrefix(['steps/editor'])
   let steps = ''
+  const featureScenarioLines = featureScenario.split('\n').slice(1)
 
   const getSteps = (file: string, fileContent: string) => {
-    const steps = fileContent.split(/(When|Then)/)
+    const stepsInFile = fileContent.split(/(When|Then)/).map((step) => {
+      // remove {string}, {number} from the the step
+      return step.replace(/{.*}/g, '')
+    })
+    // check is there any step in stepsInFile matching that in featureScenarioLines
+    // const matchedSteps = stepsInFile.filter((step) => {
+    //   return featureScenarioLines.find((line) => {
+    //     return line.includes(step)
+    //   })
+    // })
   }
   goThroughFiles(dirs, getSteps)
   return steps
